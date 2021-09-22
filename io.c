4d94dcd86ecd151b1f98041dabbf6e79c03f1d70
Mark Daniel G. Dacer <markdacer21@gmail.com>
2021-09-22 11:40:22 +0800

    Update io.c

--- io.c
+++ io.c
@@ -3,12 +3,13 @@ int main()
 {
     int testInteger;
     float num1;
+    
     printf("Enter an integer: ");
-    scanf("%d", &testInteger);  
+    scanf("%d", &testInteger); //inputting integer to variable testInteger 
   
   
     printf("\nEnter a Floating-point Number: ");
-    scanf("%f", &num1);
+    scanf("%f", &num1); //inputting float to variable num1
 
     printf("testInteger = %d\n", testInteger);
     printf("num1 = %f", num1);
