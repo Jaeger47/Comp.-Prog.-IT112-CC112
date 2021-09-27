2bbfcec6d64d36c85998e563f8c9a046b08b36db
Mark Daniel G. Dacer <markdacer21@gmail.com>
2021-09-27 19:40:21 +0800

    Create else_if.c

---
+++ Conditional/else_if.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int time = 15;
+    if (time < 10) {
+      System.out.println("Good morning.");
+    } else if (time < 20) {
+      System.out.println("Good day.");
+    }  else {
+      System.out.println("Good evening.");
+    }
+  return 0;
+}
